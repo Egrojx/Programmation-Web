daf60c7b46c376327d7ac0b955704354c5434f85
Egrojx <jorge-andres.yepes.1@ens.etsmtl.ca>
2024-06-11 16:26:07 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,34 +1,32 @@
 <?php
 session_start();
 
-$servername = "db";
-$username = "user";
-$password = "password";
-$dbname = "Recettes";
+$host = 'db';
+$db = 'mydatabase';
+$user = 'user';
+$pass = 'password';
+$charset = 'utf8mb4';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 
-function liste_types_plats($conn) {
-    $sql = "SELECT * FROM TypePlat";
-    $result = $conn->query($sql);
-    $types_plats = [];
-    while($row = $result->fetch_assoc()) {
-        $types_plats[] = $row;
-    }
-    return $types_plats;
+function liste_types_plats($pdo) {
+    $stmt = $pdo->query('SELECT * FROM types_plats');
+    return $stmt->fetchAll();
 }
 
-function liste_types_cuisines($conn) {
-    $sql = "SELECT * FROM TypeCuisine";
-    $result = $conn->query($sql);
-    $types_cuisines = [];
-    while($row = $result->fetch_assoc()) {
-        $types_cuisines[] = $row;
-    }
-    return $types_cuisines;
+function liste_types_cuisines($pdo) {
+    $stmt = $pdo->query('SELECT * FROM types_cuisines');
+    return $stmt->fetchAll();
 }
 ?>
